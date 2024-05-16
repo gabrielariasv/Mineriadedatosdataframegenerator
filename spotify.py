7db890d628986ec95199360fafc3a232c5abc1a1
Gabriel <gabriel.arias@usm.cl>
2024-05-15 20:21:29 -0400

    commit inicial

---
+++ spotify.py
@@ -0,0 +1,42 @@
+import requests
+import csv
+from datetime import date
+import os
+
+def get_spotify_token():
+    data = {
+        'grant_type': 'client_credentials',
+        'client_id': 'f1a32a13688c49a7885b79ac682094b1',
+        'client_secret': '05cdde99caaa40c6afb8c9c5aacb528a'
+    }
+
+    response = requests.post('https://accounts.spotify.com/api/token', data=data)
+    return response.json().get('access_token')
+
+def get_spotify_data(accessToken):
+    headers = { 
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': f'Bearer {accessToken}'
+    }
+
+    response = requests.get('https://api.spotify.com/v1/playlists/37i9dQZEVXbMDoHDwVN2tF', headers=headers)
+    data = response.json()
+
+    currentDate = date.today().isoformat() # Obtiene la fecha actual en formato YYYY-MM-DD
+    top50 = [{
+        'position': index + 1, # Añade la posición de la pista en la playlist
+        'trackName': item['track']['name'],
+        'trackId': item['track']['id'], # Añade la ID del track
+        'artists': '; '.join([artist['name'] for artist in item['track']['artists']]), # Añade todos los artistas
+        'date': currentDate # Añade la fecha del día
+    } for index, item in enumerate(data['tracks']['items'])]
+
+    file_exists = os.path.isfile('data/spotifyData.csv')
+    with open('data/spotifyData.csv', 'a', newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=top50[0].keys())
+        if not file_exists:
+            writer.writeheader()  # Si el archivo no existe, escribe los nombres de las columnas
+        writer.writerows(top50)
+
+    return 'Datos guardados correctamente'
---
+++ tiktok.py
@@ -0,0 +1,65 @@
+from TikTokApi import TikTokApi
+import csv
+from datetime import date
+import os
+import asyncio
+
+ms_token = os.environ.get("Y8tXeEIb0ML1y9Kl4sfChd63KBLt9-dvemIuWNPv1V6MGToVlYg3ErOChI4O_MUxBE-1eRzzz7TurwmPcwLjQt3MGRpDZeStUgK4T170ALpnZgQKOzRXNTzpY4hA", None) 
+async def trending_videos(count):
+    currentDate = date.today().isoformat() # Obtiene la fecha actual en formato YYYY-MM-DD
+    index = 0
+    result = []
+    async with TikTokApi() as api:
+        await api.create_sessions(ms_tokens = [ms_token], num_sessions=(int(count/30)), sleep_after=9, headless = False )
+        for _ in range(int(count/30)):
+            async for video in api.trending.videos(count=30):
+                tiktok = video.as_dict
+                top50 = {
+                'position': index + 1, # Añade la posición del video en la lista
+                'title': tiktok['desc'],
+                'user': tiktok['author']['nickname'] + "; " +tiktok['author']['id'] ,
+                'views': tiktok['stats']['playCount'],
+                'likes': tiktok['stats']['diggCount'],
+                'comments':tiktok['stats']['commentCount'],
+                'original_music': tiktok['music']['original'],
+                'musicTitle': tiktok['music']['title'],
+                'musicId': tiktok['music']['id'],
+                'date': currentDate # Añade la fecha del día
+                }
+                result.append(top50)
+                index+=1
+    return result
+
+async def get_tiktok_data(count):
+    currentDate = date.today().isoformat() # Obtiene la fecha actual en formato YYYY-MM-DD
+    results = []
+    index = 0
+    async with TikTokApi() as api:
+        await api.create_sessions(ms_tokens = [ms_token], num_sessions=(int(count/15)), sleep_after=3, headless = False )
+
+        for _ in range(int(count/15)):
+            async for video in api.trending.videos(count=50):
+                tiktok = video.as_dict
+                top50 = {
+                'position': index + 1, # Añade la posición del video en la lista
+                'title': tiktok['desc'],
+                'user': tiktok['author']['uniqueId'],
+                'views': tiktok['stats']['playCount'],
+                'likes': tiktok['stats']['diggCount'],
+                'comments': tiktok['stats']['commentCount'],
+                'music': tiktok['music']['title'],
+                'videoUrl': tiktok['video']['downloadAddr'],
+                'musicId': tiktok['music']['id'],
+                'date': currentDate # Añade la fecha del día
+                }
+                index+=1
+                results.append(top50)
+
+    return results
+
+def tiktokcsv(results):
+    with open('data/tiktokData.csv', 'a',  newline='', encoding='utf-8') as f:
+        writer = csv.DictWriter(f, fieldnames=results[0].keys())
+        if f.tell() == 0:
+            writer.writeheader()  # Si el archivo está vacío, escribe los nombres de las columnas
+        writer.writerows(results)
